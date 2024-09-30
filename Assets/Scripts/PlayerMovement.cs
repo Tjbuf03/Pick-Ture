a867caf7dbc0a9d81b5be40af603da34c35b16f4
AnthonyJRusso <russoanthony621@gmail.com>
2024-09-30 19:57:14 -0400

    Groundcheck on the jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,6 +16,8 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        onGround = true;
     }
 
     // Update is called once per frame
@@ -25,9 +27,31 @@ public class PlayerMovement : MonoBehaviour
         float x_value = (Input.GetAxisRaw("Horizontal") * speed);
         transform.position += new Vector3(x_value * Time.deltaTime, 0f, 0f);
 
-        //Jump with groundcheck
-        if(Input.GetKeyDown(KeyCode.Space)){
+        //Jump with groundcheck, if on ground bool is true
+        if(Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
             rb.AddForce(new Vector2(rb.velocity.x, jump * 10));
+            onGround = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other){
+        //When player collides with ground set bool to true
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            Vector3 normal = other.GetContact(0).normal;
+            if(normal == Vector3.up)
+            {
+                onGround = true;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other){
+        //When player collides with ground set bool to true
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            onGround = false;
         }
     }
 }
