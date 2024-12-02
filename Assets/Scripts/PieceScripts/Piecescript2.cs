5358d3896ce4add0a707a4417187cfa28964f482
AnthonyJRusso <russoanthony621@gmail.com>
2024-12-01 23:51:29 -0500

    Made pieces single instance

--- Assets/Scripts/PieceScripts/Piecescript2.cs
+++ Assets/Scripts/PieceScripts/Piecescript2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Piecescript : MonoBehaviour
+public class Piecescript2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -21,7 +21,11 @@ public class Piecescript : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Destroy(gameObject);
+
+            MainManager.Instance.Collected2 = true;
+
+            //Adds to piece count UI
+            MainManager.Instance.PieceNumber++;
         }
     }
-
 }
