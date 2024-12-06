92a6ddbbbd192f91e33c69572beea67f825e503f
Tjbuf03 <thomasbufalini54@gmail.com>
2024-12-05 23:17:52 -0500

    playtest fixings and music
    - Reduced the amount of text in cannon info
    - Made cannon platforms thicker
    - Removed W to zoom text and put it in start screen
    - Added music to all the scenes

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Make this GameObject persist across scenes
+        }
+        else
+        {
+            Destroy(gameObject); // Destroy duplicate instances
+        }
+    }
+    void Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        if (!audio.isPlaying)
+        {
+            audio.Play();
+        }
+    }
+
+}
+
