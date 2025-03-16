2dd716aa0f77cdbdd0b914e4e59408299e373315
AnthonyJRusso <russoanthony621@gmail.com>
2025-03-16 19:42:13 -0400

    Music lowers on pause menu

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -4,18 +4,18 @@ public class MusicManager : MonoBehaviour
 {
     private static MusicManager instance;
 
-    void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject); // Make this GameObject persist across scenes
-        }
-        else
-        {
-            Destroy(gameObject); // Destroy duplicate instances
-        }
-    }
+    //void Awake()
+    //{
+       // if (instance == null)
+        //{
+        //    instance = this;
+         //   DontDestroyOnLoad(gameObject); // Make this GameObject persist across scenes
+        //}
+        //else
+        //{
+        //    Destroy(gameObject); // Destroy duplicate instances
+        //}
+    //}
     void Start()
     {
         AudioSource audio = GetComponent<AudioSource>();
